ee17c98d2ae4fa8243aecddf7acd6dd0bbf7e92b
NorthernL1ghts <137669199+AthithIttiphakorn@users.noreply.github.com>
2025-07-09 03:38:13 +0000

    Testing out go_xyz_speed() cmd to move to mission pad location

--- scanPad+TakePhoto.py
+++ scanPad+TakePhoto.py
@@ -1,26 +1,53 @@
 from preflight import preFlight
+import time
 
 
 def scanAndPhoto(d):
     
     padId = []
+    #Hard coded coordinates (in cm) to avoid
+    avoid = [ [0, 105], [180, 105], [0, 185], [180, 185] ]
+
+    #hardcoded height of qr
+    qrHeight = 75 - d.get_height()
 
     d.takeoff()
     d.enable_mission_pads()
-    d.move_up(75 - d.get_height())
+
+    d.move_up(qrHeight)
+
+    pads = list(map(int, input().split(" ")))
+
+    completed = False
+
+    for pad in pads:
+        d.go_xyz_speed(0,0,0, 30, pad)
+        time.sleep(1)
 
 
-    for i in range(4):
-        #if d.get_mission_pad_id() == -1:
-        d.move_forward(80)
-        padId.append(d.get_mission_pad_id())
-        if i == 3:
-            d.rotate_clockwise(90)
-            takePic(d)
-            d.rotate_counter_clockwise(90)
-        else:
-            takePic(d)
 
-        d.rotate_counter_clockwise(90)
-        d.move_forward(90)
-    d.land()
+
+
+
+
+
+
+
+
+    # for i in range(4):
+    #     #if d.get_mission_pad_id() == -1:
+    #     d.move_forward(80)
+    #     padId.append(d.get_mission_pad_id())
+    #     if i == 3:
+    #         d.rotate_clockwise(90)
+    #         takePic(d)
+    #         d.rotate_counter_clockwise(90)
+    #     else:
+    #         takePic(d)
+
+    #     d.rotate_counter_clockwise(90)
+    #     d.move_forward(90)
+    # d.land()
+
+
+
